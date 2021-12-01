9f655f21979ec4ebcbfc5b002c6c5107ce00294a
jassAbidi <jassAbidi>
2021-12-01 19:04:34 +0000

    remove cell

--- Code1.py
+++ Code1.py
@@ -2,7 +2,3 @@
 def generate_data1(n=1000, name='my_cool_data'):
   df = spark.range(0, n)
   df.createOrReplaceTempView(name)
-
-# COMMAND ----------
-
-# just a change of the code to trigger release
